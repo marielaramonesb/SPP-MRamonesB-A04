e3ac6e027709de0c20f5466a71d313e70714fcec
marielaramonesbalvoa <marielaramonesbalvoa@10.34.157.204>
2017-02-01 14:23:45 -0600

    Funciones Trigonometricas

--- src/spp/mramonesb/a04/SPPMRamonesBA04.java
+++ src/spp/mramonesb/a04/SPPMRamonesBA04.java
@@ -1,3 +1,4 @@
+// MARIELA RAMONES BALVOA- A01411449- IMT
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
